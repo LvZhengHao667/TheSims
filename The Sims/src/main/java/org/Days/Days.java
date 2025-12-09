8e27330dc7a5c04d220860a659ffbc724878266b
LvZhengHao667 <3372997051@qq.com>
2025-12-09 19:01:01 +0800

    Add files via upload

---
+++ The Sims/src/main/java/org/Days/Days.java
@@ -0,0 +1,12 @@
+package org.Days;
+
+public class Days {
+    //Days
+    int day = 0;
+    public void setDay(){
+        this.day+=1;
+    }
+    public int getDay(){
+        return this.day;
+    }
+}
